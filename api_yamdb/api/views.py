3f8283b357164f59624d8d08ddcdf11383214ed5
Михаил Васильев <combaser@mail.ru>
2022-07-28 21:14:09 +0500

    Fixing isorts again...

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,7 +10,6 @@ from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title, User
 
 from .filters import TitleFilter
